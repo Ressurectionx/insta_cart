967dbce3b79c2aaf43d7693a16615949b2d59a06
Rajjo <56448015+Ressurectionx@users.noreply.github.com>
2022-07-08 22:33:54 +0530

    Add product model

---
+++ lib/src/data/models/product_model.dart
@@ -0,0 +1,64 @@
+class ProductModel {
+  List<Products>? products;
+  int? total;
+  int? skip;
+  int? limit;
+
+  ProductModel(
+      {required this.products,
+        required this.total,
+        required this.skip,
+        required this.limit});
+
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    if (json['products'] != null) {
+      products = <Products>[];
+      json['products'].forEach((v) {
+        products?.add(new Products.fromJson(v));
+      });
+    }
+    total = json['total'];
+    skip = json['skip'];
+    limit = json['limit'];
+  }
+}
+class Products {
+  int? id;
+  String? title;
+  String? description;
+  int? price;
+  double? discountPercentage;
+  double? rating;
+  int? stock;
+  String? brand;
+  String? category;
+  String? thumbnail;
+  List<String>? images;
+
+  Products(
+      {required this.id,
+        required this.title,
+        required this.description,
+        required this.price,
+        required this.discountPercentage,
+        required this.rating,
+        required this.stock,
+        required this.brand,
+        required this.category,
+        required this.thumbnail,
+        required this.images});
+
+  Products.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    description = json['description'];
+    price = json['price'];
+    discountPercentage = json['discountPercentage'].toDouble();
+    rating = json['rating'].toDouble();
+    stock = json['stock'];
+    brand = json['brand'];
+    category = json['category'];
+    thumbnail = json['thumbnail'];
+    images = json['images'].cast<String>();
+  }
+}
