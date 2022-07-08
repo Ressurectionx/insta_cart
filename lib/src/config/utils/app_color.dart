c66eb2d998907089c103df7a9aedf67882ba2ea3
Rajjo <56448015+Ressurectionx@users.noreply.github.com>
2022-07-08 22:18:43 +0530

    Add Folder Structure
    This commit will add folder structure

---
+++ lib/src/config/utils/app_color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Color blue = const Color(0xff0C80D7);
+Color greenLight = const Color(0xff61ee02);
+Color green = const Color(0xff44a802);
+Color greenDark = const Color(0xff44a802);
+Color blueLight = const Color(0xffDBF4FF);
+Color blueDark = const Color(0xff01497A);
+Color lightRed = const Color(0xaed70c0c);
+Color orange = const Color(0xffff4400);
+Color purple = const Color(0xFF4A3298);
+Color grey = const Color(0xffababab);
+Color darkGrey = const Color(0xff7a7878);
+Color greyLight = const Color(0xffdbdcdc);
+Color black = const Color(0xff000000);
+Color white = const Color(0xffffffff);
+Color transparent = Colors.transparent;
